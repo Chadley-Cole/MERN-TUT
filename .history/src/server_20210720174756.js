c0fe079c552b498caa588fa8eaf9fa155fec1513
Chadley <chadley-cole@hotmail.co.uk>
2021-07-20 17:49:36 +0100

    Initial commit

---
+++ .history/src/server_20210720174756.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (res, req)=> {})
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
---
+++ .history/src/server_20210720174803.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (res, req)=> {
+    res.setEncoding
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
